0b8b0c32e0423084d289e85b7ef24ec50aec53c5
timlin <1103137241@gm.kuas.edu.tw>
2017-04-12 18:27:35 +0800

    Add project files.

---
+++ esales/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(esales.Startup))]
+namespace esales
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
